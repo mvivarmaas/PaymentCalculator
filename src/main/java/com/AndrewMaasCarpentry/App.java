2afb257d21209bc954f88f03b39c6eed6b355d37
Max Vivar-Maas <mvivarmaas@gmail.com>
2020-05-12 10:59:30 -0400

    Initial commit

---
+++ src/main/java/com/AndrewMaasCarpentry/App.java
@@ -0,0 +1,13 @@
+package com.AndrewMaasCarpentry;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
